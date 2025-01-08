252fb3f618fe53fb17b8cebd3e12267977ccad21
Dennis Muturi <deynohmuturi@gmail.com>
2025-01-08 19:38:41 +0300

    Enhance food recommendation model by loading recipe data from CSV and updating nearest neighbor model to use sample embeddings; return recipe recommendations as a list of dictionaries

--- ML_and_Data_Science/food_recommendation.py
+++ ML_and_Data_Science/food_recommendation.py
@@ -5,11 +5,14 @@ import numpy as np
 from sklearn.neighbors import NearestNeighbors
 from sklearn.feature_extraction.text import TfidfVectorizer
 from collections import defaultdict
+import ast
 
+
+recipe_data=pd.read_csv('./datasets/final_2M_sample_recipes.csv')
+recipe_data['ingredients']=recipe_data['ingredients'].apply(ast.literal_eval)
+recipe_data['directions']=recipe_data['directions'].apply(ast.literal_eval)
 with open('./datasets/ML Models/TFIDF/tfidf_vectorizer.pkl', 'rb') as file:
     tfidf_vectorizer = pickle.load(file)
-
-# print(tfidf_vectorizer.vocabulary_.items())
 model = Word2Vec.load("./datasets/ML Models/Word2VecModel/model_cbow3.bin")
 print(model.wv.most_similar("salt"))
 max_idf = max(tfidf_vectorizer.idf_)
@@ -38,12 +41,14 @@ def doc_average_list(docs):
 
 # ingredientsEmbeddings = np.load('/content/drive/MyDrive/archive/embeddings.npy')
 nn=NearestNeighbors(algorithm='brute',metric='cosine')
-nearestNeighborModel=nn.fit(np.load('./datasets/ML Models/Ingredients_embeddings/embeddings.npy'))
+nearestNeighborModel=nn.fit(np.load('./datasets/ML Models/Ingredients_embeddings/sample_embeddings.npy'))
 
 def makePrediction(ingredientsList):
   ingredientsList_embedding=doc_average(ingredientsList).reshape(1,-1)
   distances,indices=nearestNeighborModel.kneighbors(ingredientsList_embedding,n_neighbors=10)
-  return indices[0]
+  recipes_recommendation = recipe_data.iloc[indices[0],]
+  recipes_recommendation_list = recipes_recommendation.to_dict(orient='records')
+  return recipes_recommendation_list
 
 if __name__=='__main__':
   inputs=['chicken thigh', 'onion', 'rice noodle', 'seaweed nori sheet', 'sesame', 'shallot', 'soy', 'spinach', 'star', 'tofu']
