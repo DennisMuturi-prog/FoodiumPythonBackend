ad20634356eb5b50c51847b00dca893995e1e5e4
Dennis Muturi <deynohmuturi@gmail.com>
2025-01-12 15:48:24 +0300

    Refactor Kenyan recipe recommendation model to improve neighbor search efficiency and handle missing data

--- ML_and_Data_Science/food_recommendation.py
+++ ML_and_Data_Science/food_recommendation.py
@@ -14,11 +14,13 @@ recipe_data['ingredients']=recipe_data['ingredients'].apply(ast.literal_eval)
 recipe_data['directions']=recipe_data['directions'].apply(ast.literal_eval)
 kenyan_recipe_data['parsedIngredientsList']=kenyan_recipe_data['parsedIngredientsList'].apply(ast.literal_eval)
 kenyan_recipe_data=kenyan_recipe_data.fillna('')
+recipe_data=recipe_data.fillna('')
 with open('./datasets/ML Models/TFIDF/tfidf_vectorizer.pkl', 'rb') as file:
     tfidf_vectorizer = pickle.load(file)
 with open('./datasets/ML Models/TFIDF/kenyan_tfidf_vectorizer.pkl', 'rb') as file:
     kenyan_tfidf_vectorizer = pickle.load(file)
 model = Word2Vec.load("./datasets/ML Models/Word2VecModel/worldwide/model_cbow3.bin")
+print(model.wv.most_similar("salt"))
 kenyan_model = Word2Vec.load("./datasets/ML Models/Word2VecModel/kenyan/kenyan_model_cbow3.bin")
 max_idf = max(tfidf_vectorizer.idf_)
 word_idf_weight = defaultdict(
@@ -68,18 +70,19 @@ def kenyan_doc_average_list(docs):
 # ingredientsEmbeddings = np.load('/content/drive/MyDrive/archive/embeddings.npy')
 nn=NearestNeighbors(algorithm='brute',metric='cosine')
 nearestNeighborModel=nn.fit(np.load('./datasets/ML Models/Ingredients_embeddings/sample_embeddings.npy'))
-kenyanNearestNeighborModel=nn.fit(np.load('./datasets/ML Models/Ingredients_embeddings/matched_Kenyan_embeddings.npy'))
+kenyan_nn=NearestNeighbors(algorithm='brute',metric='cosine')
+kenyanNearestNeighborModel=kenyan_nn.fit(np.load('./datasets/ML Models/Ingredients_embeddings/matched_Kenyan_embeddings.npy'))
+# kenyanNearestNeighborModel=nn.fit(np.load('./datasets/ML Models/Ingredients_embeddings/matched_Kenyan_embeddings.npy'))
 def makePrediction(ingredientsList:list[str],dataset:str='world'):
-  print(ingredientsList)
   if(dataset=='Kenyan'):
     ingredientsList_embedding=kenyan_doc_average(ingredientsList).reshape(1,-1)
-    distances,indices=kenyanNearestNeighborModel.kneighbors(ingredientsList_embedding,n_neighbors=10)
+    distances,indices=kenyanNearestNeighborModel.kneighbors(ingredientsList_embedding,n_neighbors=5)
     recipes_recommendation = kenyan_recipe_data.iloc[indices[0],]
     recipes_recommendation_list = recipes_recommendation.to_dict(orient='records')
-    return recipes_recommendation_list  
+    return recipes_recommendation_list
   else:
     ingredientsList_embedding=doc_average(ingredientsList).reshape(1,-1)
-    distances,indices=nearestNeighborModel.kneighbors(ingredientsList_embedding,n_neighbors=10)
+    distances,indices=nearestNeighborModel.kneighbors(ingredientsList_embedding,n_neighbors=5)
     recipes_recommendation = recipe_data.iloc[indices[0],]
     recipes_recommendation_list = recipes_recommendation.to_dict(orient='records')
     return recipes_recommendation_list
